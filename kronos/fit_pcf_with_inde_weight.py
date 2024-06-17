bc008c4c06a020f1095919b73a994d1931da7677
abram <abram.wang@outlook.com>
2024-06-17 13:01:25 +0800

    更新拟合pcf脚本

--- kronos/fit_pcf_with_inde_weight.py
+++ kronos/fit_pcf_with_inde_weight.py
@@ -2,6 +2,7 @@ import pymoneta.quote as quote
 import pymoneta.data as data
 import pandas as pd
 import numpy as np
+from datetime import datetime
 from sklearn.linear_model import LinearRegression
 from sqlalchemy import create_engine
 
@@ -15,12 +16,30 @@ port = '20000'  # MySQL默认端口
 # 创建数据库引擎
 ti_data_engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}:{port}/ti_data')
 pcf_engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}:{port}/pcf')
+today = datetime.now().strftime('%Y%m%d')
 
-def get_etf_list():
-    pass
+def get_etf_code_list():
+    etf_info_df = pd.read_sql(f"SELECT * FROM xt_etf_info WHERE m_tradeDate = '{today}'", pcf_engine)
+    etf_info_df.rename(columns={'m_fundId': 'symbol', 'm_exchange':'exchange'}, inplace=True)
+   
+
+    etf_instruments_df = pd.read_sql("SELECT * FROM rq_base_etf_instruments WHERE status ='Active'", ti_data_engine)
+    etf_instruments_df = etf_instruments_df[["symbol","exchange", "order_book_id", "underlying_order_book_id", "underlying_symbol", "underlying_exchange"]]
+    
+    etf_info_merge_df = pd.merge(etf_info_df, etf_instruments_df, on=["symbol", "exchange"], how="left")
+    
+    etf_code_list = []
+    for index, row in etf_info_merge_df.iterrows():
+        etf_code_list.append({
+            "symbol": row["symbol"],
+            "exchange": row["exchange"],
+            "order_book_id": row["order_book_id"]
+        })
+    print(len(etf_code_list), etf_code_list[0], etf_code_list[-1])
+    return etf_code_list
 
 def main():
-    get_etf_list()
+    get_etf_code_list()
     pass
 
 if __name__ == "__main__":
