9f1a7c433534bb6ea94509935c7292df1082968b
abram.wang <abram.wang@outlook.com>
2024-07-01 22:00:54 +0800

    更新分钟线计算

---
+++ resource/src/ti_min_bar.cpp
@@ -0,0 +1,46 @@
+#include "ti_min_bar.h"
+
+TiMinBar::TiMinBar()
+{
+    m_cyc = TI_BarCycType_1m;
+};
+
+TiMinBar::TiMinBar(TI_BarCycType cyc)
+{
+    m_cyc = cyc;
+};
+
+void TiMinBar::processTrade(const int64_t timestamp,  const double value)  {
+    //  获取当前分钟的时间戳
+    int64_t time_mod  =  timestamp % (int(m_cyc) * 60 * 1000);
+    int64_t currentMinute_time = timestamp - time_mod;
+
+    //  查找当前分钟是否有bar数据
+    auto  it  =  m_bars.find(currentMinute_time);
+    if  (it  !=  m_bars.end())  {
+        //  更新OHLC  bar数据
+        it->second->high  = std::max(it->second->high,  value);
+        it->second->low  =  std::min(it->second->low,  value);
+        it->second->close  = value;
+        it->second->num++;
+    }  else  {
+        //  创建新的OHLC  bar数据
+        TiBarDataPtr newBar = std::make_shared<TiBarData>();
+        newBar->open    = value;
+        newBar->high    = value;
+        newBar->low     = value;
+        newBar->close   = value;
+        newBar->num     = 1;
+        newBar->timestamp  =  currentMinute_time;
+
+        m_bars[currentMinute_time]  =  newBar;
+    }
+};
+
+std::vector<TiBarDataPtr> TiMinBar::getBars()  {
+    std::vector<TiBarDataPtr>  result;
+    for  (const  auto&  bar  :  m_bars)  {
+        result.push_back(bar.second);
+    }
+    return result;
+}
--- resource/src/ti_min_bar.h
+++ resource/src/ti_min_bar.h
@@ -6,7 +6,6 @@
 #include <vector>
 #include <map>
 
-
 const char TI_BarCycType_1m         = 1;    // 1 分钟
 const char TI_BarCycType_5m         = 5;    // 5 分钟
 const char TI_BarCycType_10m        = 10;   // 卖出
@@ -30,48 +29,19 @@ typedef std::shared_ptr<TiBarData> TiBarDataPtr;
 class TiMinBar {
 private:
     std::map<int64_t,  TiBarDataPtr>  m_bars;  //  存储OHLC  bar数据的map
+    TI_BarCycType m_cyc;
 
 public:
     TiMinBar();
-    ~TiMinBar();
+    TiMinBar(TI_BarCycType cyc);
+    ~TiMinBar(){};
 
 
     //  处理成交数据
-    void  processTrade(const int64_t timestamp,  const double value)  {
-        //  获取当前分钟的时间戳
-        int64_t time_mod  =  timestamp % (60 * 1000);
-        int64_t currentMinute_time = timestamp - time_mod;
-
-        //  查找当前分钟是否有bar数据
-        auto  it  =  m_bars.find(currentMinute_time);
-        if  (it  !=  m_bars.end())  {
-            //  更新OHLC  bar数据
-            it->second->high  = std::max(it->second->high,  value);
-            it->second->low  =  std::min(it->second->low,  value);
-            it->second->close  = value;
-            it->second->num++;
-        }  else  {
-            //  创建新的OHLC  bar数据
-            TiBarDataPtr newBar = std::make_shared<TiBarData>();
-            newBar->open    = value;
-            newBar->high    = value;
-            newBar->low     = value;
-            newBar->close   = value;
-            newBar->num     = 1;
-            newBar->timestamp  =  currentMinute_time;
-
-            m_bars[currentMinute_time]  =  newBar;
-        }
-    }
+    void  processTrade(const int64_t timestamp,  const double value);
 
     //  获取所有生成的OHLC  bar数据
-    std::vector<TiBarDataPtr>  getBars()  {
-        std::vector<TiBarDataPtr>  result;
-        for  (const  auto&  bar  :  m_bars)  {
-            result.push_back(bar.second);
-        }
-        return  result;
-    }
+    std::vector<TiBarDataPtr>  getBars();
 
 };
 
