f736e521e325bfe18d020326ed15c26c9b2074be
abram.wang <abram.wang@outlook.com>
2024-08-03 15:42:22 +0800

    schedule 增加 glog记录日志

--- themis/scheduler.py
+++ themis/scheduler.py
@@ -2,10 +2,26 @@
 
 import schedule
 import time
+import glog as log
+import sys
 from tasks.download_rq_index import main as download_rq_index
 
+
+# 配置日志记录
+log.setLevel("INFO")
+log.handler.setStream(open('scheduler.log', 'a'))
+
+def log_task(task, task_name):
+    try:
+        log.info(f"{task_name} started")
+        task()
+        log.info(f"{task_name} completed successfully")
+    except Exception as e:
+        log.error(f"Error in {task_name}: {e}")
+
 # 安排任务在特定时间运行
-schedule.every().day.at("11:45").do(download_rq_index)
+
+schedule.every().day.at("15:42").do(log_task, download_rq_index, "Task download_rq_index")
 
 # 不断运行调度器
 while True:
--- themis/tasks/download_rq_index.py
+++ themis/tasks/download_rq_index.py
@@ -1,4 +1,15 @@
 # tasks/task1.py
+import glog as log
+
+# 配置日志记录
+log.setLevel("INFO")
+log.handler.setStream(open('scheduler.log', 'a'))
 
 def main():
     print("Task 1 executed")
+    try:
+        log.info("Task 1 is executing")
+        # 你的任务代码
+        log.info("Task 1 finished execution")
+    except Exception as e:
+        log.error(f"Error in Task 1: {e}")
