d753f8e8d89f771e5d0b2d687e05ccee9c508157
abram.wang <abram.wang@outlook.com>
2025-01-03 11:40:51 +0800

    东方增加回放程序

--- oceanus/src/oc_quote_ipc_server_dongfang/main.cpp
+++ oceanus/src/oc_quote_ipc_server_dongfang/main.cpp
@@ -3,17 +3,12 @@
 #include <memory>
 
 #include "ti_df_quote_client.h"
+#include "ti_df_trader_client.h"
 
-#include "quote_demo_lv2.h"
 
 int main(int argc, char *argv[]) {
-    TiDfQuoteClient client;
+    TiDfTraderClient client;
     client.Run();
-
-    return 0;
-    // 行情lv2相关demo
-    QuoteDemoLv2 quote_demo_lv2;
-    quote_demo_lv2.Run();
 
     return 0;
 }
