ef51f9dee3c7fac2bb14c184e1b3112d19e115bc
abram.wang <abram.wang@outlook.com>
2025-03-04 18:52:57 +0800

    修正bug

--- oceanus/src/oc_ipc_quote_depth_server/oc_quote_depth_snap_repairman.cpp
+++ oceanus/src/oc_ipc_quote_depth_server/oc_quote_depth_snap_repairman.cpp
@@ -52,14 +52,16 @@ bool OcQuoteDepthSnapRepairman::OnL2FittedStockSnapshotRtn(const TiQuoteSnapshot
     {
         m_closest_fitted_snapshot = new TiQuoteSnapshotStockField;
         memset(m_closest_fitted_snapshot, 0, sizeof(TiQuoteSnapshotStockField));
+        memcpy(m_closest_fitted_snapshot, pData, sizeof(TiQuoteSnapshotStockField));
     }
 
     if (!m_lasted_fitted_snapshot)
     {
         m_lasted_fitted_snapshot = new TiQuoteSnapshotStockField;
         memset(m_lasted_fitted_snapshot, 0, sizeof(TiQuoteSnapshotStockField));
+        memcpy(m_lasted_fitted_snapshot, pData, sizeof(TiQuoteSnapshotStockField));
     }
-    memcpy(&m_lasted_fitted_snapshot, pData, sizeof(TiQuoteSnapshotStockField));
+    memcpy(m_lasted_fitted_snapshot, pData, sizeof(TiQuoteSnapshotStockField));
 
 
     if (m_original_snapshot_stock == NULL)
@@ -72,7 +74,7 @@ bool OcQuoteDepthSnapRepairman::OnL2FittedStockSnapshotRtn(const TiQuoteSnapshot
 
     if (time_diff_lasted <= time_diff_closest)
     {
-        memcpy(&m_closest_fitted_snapshot, m_lasted_fitted_snapshot, sizeof(TiQuoteSnapshotStockField));
+        memcpy(m_closest_fitted_snapshot, m_lasted_fitted_snapshot, sizeof(TiQuoteSnapshotStockField));
 
         m_acc_volume_diff = m_original_snapshot_stock->acc_volume - m_closest_fitted_snapshot->acc_volume;
         m_acc_turnover_diff = m_original_snapshot_stock->acc_turnover - m_closest_fitted_snapshot->acc_turnover;
